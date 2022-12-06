5d80eec3ea6b8dbe526dab4fa00c5b716fd23ea9
wo80 <christian.woltering@tu-dortmund.de>
2022-12-06 18:21:22 +0100

    RogueVector: fix for VS2019 and above. The hack goes on ... *sigh*

--- src/essentia/roguevector.h
+++ src/essentia/roguevector.h
@@ -22,6 +22,18 @@
 
 #include <vector>
 #include "types.h"
+#include "pool.h"
+
+namespace {
+    // This struct is different in every translation unit.  We use template
+    // instantiations to define inline freestanding methods.  Since the
+    // methods are inline it is fine to define them in multiple translation
+    // units, but the instantiation itself would be an ODR violation if it is
+    // present in the program more than once.  By tagging the instantiations
+    // with this struct, we avoid ODR problems for the instantiation while
+    // allowing the resulting methods to be inline-able.
+    struct EssentiaVectorHackTranslationUnitTag {};
+} // namespace
 
 namespace essentia {
 
@@ -90,8 +102,121 @@ void RogueVector<T>::setData(T* data, size_t size) {
 #elif defined(_MSC_VER) && _MSC_VER > 1916
 // MSVC >= VS2019
 
+// The following implementation is based on
+// https://github.com/facebook/folly/blob/main/folly/memory/UninitializedMemoryHacks.h
+//
+// License:
+//
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace unsafe {
+
+namespace detail {
+  template <typename T>
+  void set_data(std::vector<T>& v, T* data, std::size_t n);
+} // namespace detail
+
+template <typename T>
+void set_vector_data(std::vector<T>& v, T* data, std::size_t n) {
+    detail::set_data(v, data, n);
+}
+} // namespace unsafe
+
+namespace unsafe {
+namespace detail {
+
+#define DECLARE_VECTOR_SET_DATA_IMPL(TYPE)                      \
+  namespace unsafe {                                            \
+  namespace detail {                                            \
+  void set_data_impl(std::vector<TYPE>& v, TYPE*, std::size_t); \
+  template <>                                                   \
+  inline void set_data<TYPE>(                                   \
+      std::vector<TYPE> & v, TYPE* data, std::size_t n) {       \
+    set_data_impl(v, data, n);                                  \
+  }                                                             \
+  }                                                             \
+  }
+
+// For Microsoft's STL vector implementation (VS2019+), see
+// https://github.com/microsoft/STL/blob/2f03bdf361f7f153b4216c60a0d9491c0be13a73/stl/inc/vector
+
+template <
+    typename Tag,
+    typename T,
+    typename A,
+    A Ptr_Mypair,
+    typename B,
+    B Ptr_Myval2,
+    typename C,
+    C Ptr_Mylast,
+    typename D,
+    D Ptr_Myfirst,
+    typename E,
+    E Ptr_Myend>
+struct VectorHack : std::vector<T> {
+  friend void set_data_impl(std::vector<T>& v, T* data, std::size_t n) {
+      ((v.*Ptr_Mypair).*Ptr_Myval2).*Ptr_Myfirst = data;
+      ((v.*Ptr_Mypair).*Ptr_Myval2).*Ptr_Mylast = ((v.*Ptr_Mypair).*Ptr_Myval2).*Ptr_Myfirst + n;
+      ((v.*Ptr_Mypair).*Ptr_Myval2).*Ptr_Myend = ((v.*Ptr_Mypair).*Ptr_Myval2).*Ptr_Myfirst + n;
+  }
+};
+
+#define DECLARE_VECTOR_SET_DATA(TYPE)                                          \
+  template struct unsafe::detail::VectorHack<                                  \
+      EssentiaVectorHackTranslationUnitTag,                                    \
+      TYPE,                                                                    \
+      decltype(&std::vector<TYPE>::_Mypair),                                   \
+      &std::vector<TYPE>::_Mypair,                                             \
+      decltype(&decltype(std::declval<std::vector<TYPE>>()._Mypair)::_Myval2), \
+      &decltype(std::declval<std::vector<TYPE>>()._Mypair)::_Myval2,           \
+      decltype(&decltype(std::declval<std::vector<TYPE>>()                     \
+                             ._Mypair._Myval2)::_Mylast),                      \
+      &decltype(std::declval<std::vector<TYPE>>()._Mypair._Myval2)::_Mylast,   \
+      decltype(&decltype(std::declval<std::vector<TYPE>>()                     \
+                             ._Mypair._Myval2)::_Myfirst),                     \
+      &decltype(std::declval<std::vector<TYPE>>()._Mypair._Myval2)::_Myfirst,  \
+      decltype(&decltype(std::declval<std::vector<TYPE>>()                     \
+                             ._Mypair._Myval2)::_Myend),                       \
+      &decltype(std::declval<std::vector<TYPE>>()._Mypair._Myval2)::_Myend>;   \
+  DECLARE_VECTOR_SET_DATA_IMPL(TYPE)                                           \
+
+} // namespace detail
+} // namespace unsafe
+
+// The following list emerged by trial & error:
+
+DECLARE_VECTOR_SET_DATA(int)
+DECLARE_VECTOR_SET_DATA(float)
+DECLARE_VECTOR_SET_DATA(Tuple2<float>)
+DECLARE_VECTOR_SET_DATA(std::string)
+DECLARE_VECTOR_SET_DATA(std::vector<int>)
+DECLARE_VECTOR_SET_DATA(std::vector<float>)
+DECLARE_VECTOR_SET_DATA(std::vector<std::complex<float>>)
+DECLARE_VECTOR_SET_DATA(std::vector<std::vector<float>>)
+DECLARE_VECTOR_SET_DATA(std::vector<std::vector<std::complex<float>>>)
+DECLARE_VECTOR_SET_DATA(TNT::Array2D<Real>)
+DECLARE_VECTOR_SET_DATA(Tensor<Real>)
+DECLARE_VECTOR_SET_DATA(essentia::Pool)
+// Only for testing (test_fileoutput.cpp)
+DECLARE_VECTOR_SET_DATA(std::complex<float>)
+DECLARE_VECTOR_SET_DATA(std::vector<std::string>)
+DECLARE_VECTOR_SET_DATA(TNT::Array1D<Real>)
+
 template <typename T>
 void RogueVector<T>::setData(T* data, size_t size) {
+    unsafe::set_vector_data<T>(*this, data, size);
 }
 
 #else
