fa47018852efebdf654a324f85b41a0376fc493a
wo80 <christian.woltering@tu-dortmund.de>
2024-01-17 21:03:30 +0100

    CMake: add option to build VAMP plugin.

---
+++ cmake/modules/FindVampSdk.cmake
@@ -0,0 +1,20 @@
+include(FindPackageHandleStandardArgs)
+
+if (NOT WIN32)
+    find_package(PkgConfig)
+    if (PKG_CONFIG_FOUND)
+         pkg_check_modules(VAMPSDK vamp-sdk)
+    endif ()
+endif (NOT WIN32)
+
+if ( NOT VAMPSDK_FOUND )
+  find_path(VAMPSDK_INCLUDE_DIRS NAMES vamp-sdk.h PATH_SUFFIXES vamp-sdk)
+  find_library(VAMPSDK_LIBRARIES NAMES libvamp-sdk VampPluginSDK)
+
+  if ( NOT "${VAMPSDK_LIBRARIES}" STREQUAL "")
+    set (VAMPSDK_FOUND TRUE)
+    set (VAMPSDK_LINK_LIBRARIES ${VAMPSDK_LINK_LIBRARIES} ${VAMPSDK_LIBRARIES})
+  endif ()
+endif ()
+
+find_package_handle_standard_args(VampSdk DEFAULT_MSG VAMPSDK_LIBRARIES VAMPSDK_INCLUDE_DIRS)
