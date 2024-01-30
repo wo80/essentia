7e78dee86bab2a52ce3e672440bb1364b4f9d68f
wo80 <christian.woltering@tu-dortmund.de>
2024-01-31 00:20:01 +0100

    CMake: updates to python wheel * copy python module files * remove unsupported features * cleanup

--- cmake/modules/python-wheel.cmake
+++ cmake/modules/python-wheel.cmake
@@ -1,8 +1,6 @@
 include(python-wheel-globals)
 
 set(PY_WHEEL_SETUP_FILE "${CMAKE_CURRENT_LIST_DIR}/setup.py.in" CACHE INTERNAL "")
-set(TEST_WHEEL_BASH "${CMAKE_CURRENT_LIST_DIR}/test-wheel.bash" CACHE INTERNAL "")
-set(PY_CHANGE_TAG_FILE "${CMAKE_CURRENT_LIST_DIR}/change-wheel-tag-macos.py" CACHE INTERNAL "")
 
 # Target for building all added wheels
 add_custom_target(wheel ALL)
@@ -41,7 +39,7 @@ function (add_wheel WHEEL_TARGET)
   cmake_parse_arguments(WHEEL
     ""
     "NAME;AUTHOR;EMAIL;URL;PYTHON_REQUIRES;VERSION;DESCRIPTION;KEYWORDS;LICENSE;PLATFORMS;README_PATH;LICENSE_PATH"
-    "TARGET_DEPENDENCIES;MODULE_DEPENDENCIES;DEPLOY_FILES;SUBMODULES" ${ARGN})
+    "TARGET_DEPENDENCIES;MODULE_DEPENDENCIES;DEPLOY_PATH;DEPLOY_FILES;SUBMODULES" ${ARGN})
 
   to_python_list_string(WHEEL_MODULE_DEPENDENCIES WHEEL_MODULE_DEPENDENCIES_PYLIST)
 
@@ -94,15 +92,17 @@ function (add_wheel WHEEL_TARGET)
   file(REMOVE_RECURSE "${WHEEL_LIB_DIR}")
   file(MAKE_DIRECTORY "${WHEEL_LIB_DIR}")
   file(MAKE_DIRECTORY "${WHEEL_PACKAGE_DIR}")
-  file(WRITE "${WHEEL_PACKAGE_DIR}/__init__.py" "from .${WHEEL_TARGET} import *")
-  foreach(SUBMODULE IN LISTS WHEEL_SUBMODULES)
-    string(REPLACE "." "/" SUBMODULE_DIR ${SUBMODULE})
-    file(MAKE_DIRECTORY "${WHEEL_PACKAGE_DIR}/${SUBMODULE_DIR}")
-    # Create repeating dots by removing all non-dot characters from the submodule string
-    string(REGEX REPLACE "[^\\.]" "" DOTS ${SUBMODULE})
-    # Correctly adjust the import statement considering depth
-    file(WRITE "${WHEEL_PACKAGE_DIR}/${SUBMODULE_DIR}/__init__.py" "from .${DOTS}.${WHEEL_TARGET}.${SUBMODULE} import *")
-  endforeach()
+  if (WHEEL_WRITE_INIT)
+    file(WRITE "${WHEEL_PACKAGE_DIR}/__init__.py" "from .${WHEEL_TARGET} import *")
+    foreach(SUBMODULE IN LISTS WHEEL_SUBMODULES)
+      string(REPLACE "." "/" SUBMODULE_DIR ${SUBMODULE})
+      file(MAKE_DIRECTORY "${WHEEL_PACKAGE_DIR}/${SUBMODULE_DIR}")
+      # Create repeating dots by removing all non-dot characters from the submodule string
+      string(REGEX REPLACE "[^\\.]" "" DOTS ${SUBMODULE})
+      # Correctly adjust the import statement considering depth
+      file(WRITE "${WHEEL_PACKAGE_DIR}/${SUBMODULE_DIR}/__init__.py" "from .${DOTS}.${WHEEL_TARGET}.${SUBMODULE} import *")
+    endforeach()
+  endif()
 
   # Only one wheel allowed per project.
   file(GLOB LOCAL_WHEEL_LIST "${CMAKE_CURRENT_BINARY_DIR}/*.wheel")
@@ -138,6 +138,11 @@ function (add_wheel WHEEL_TARGET)
       COMMAND ${CMAKE_COMMAND} -E copy "${WHEEL_README_PATH}" "${WHEEL_PACKAGE_DIR}/README.txt")
   endif()
 
+  if (WHEEL_DEPLOY_PATH)
+    get_filename_component(WHEEL_PACKAGE_DIR_PARENT "${WHEEL_PACKAGE_DIR}" PATH)
+    file(COPY "${WHEEL_DEPLOY_PATH}" DESTINATION "${WHEEL_PACKAGE_DIR_PARENT}/")
+  endif()
+
   if (WHEEL_DEPLOY_FILES)
     foreach (file IN LISTS WHEEL_DEPLOY_FILES)
       add_custom_command(TARGET ${WHEEL_TARGET}-copy-files
@@ -150,42 +155,14 @@ function (add_wheel WHEEL_TARGET)
   set(SETUP_FILE "${CMAKE_CURRENT_BINARY_DIR}/setup.py")
   configure_file("${PY_WHEEL_SETUP_FILE}" "${SETUP_FILE}")
 
-  if(APPLE)
-    set(EXTRA_ARGS COMMAND "${Python3_EXECUTABLE}" "${PY_CHANGE_TAG_FILE}" "${WHEEL_DEPLOY_DIRECTORY}" "${WHEEL_NAME}")
-  else()
-    set(EXTRA_ARGS "")
-  endif()
-
   add_custom_target(${WHEEL_TARGET}-setup-py
     COMMAND
       "${Python3_EXECUTABLE}" "-m" "pip" "wheel"
         "${CMAKE_CURRENT_BINARY_DIR}"
         "--no-deps"
-        "-w" "${WHEEL_DEPLOY_DIRECTORY}"
-      ${EXTRA_ARGS})
+        "-w" "${WHEEL_DEPLOY_DIRECTORY}")
 
   add_dependencies(${WHEEL_TARGET}-setup-py ${WHEEL_TARGET}-copy-files ${WHEEL_TARGET})
 
   add_dependencies(wheel ${WHEEL_TARGET}-setup-py)
-endfunction()
-
-function (add_wheel_test TEST_NAME)
-  set(Python_FIND_VIRTUALENV FIRST) # Favor venv over system install
-  find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
-
-  # Parse arguments
-  cmake_parse_arguments(WHEEL_TEST
-    ""
-    "WORKING_DIRECTORY"
-    "COMMANDS" ${ARGN})
-
-  add_test(
-    NAME
-      ${TEST_NAME}
-    WORKING_DIRECTORY
-      "${WHEEL_TEST_WORKING_DIRECTORY}"
-    COMMAND
-      bash "${TEST_WHEEL_BASH}"
-        -w "${WHEEL_DEPLOY_DIRECTORY}"
-        ${WHEEL_TEST_COMMANDS})
 endfunction()
