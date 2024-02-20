d083eb9158d61176f8180da48398639c51ed214f
wo80 <christian.woltering@tu-dortmund.de>
2024-02-20 07:38:36 +0100

    CMake: fix Gaia2 module lookup.

--- cmake/modules/FindGaia2.cmake
+++ cmake/modules/FindGaia2.cmake
@@ -3,23 +3,18 @@ include(FindPackageHandleStandardArgs)
 if (NOT WIN32)
     find_package(PkgConfig)
     if (PKG_CONFIG_FOUND)
-         pkg_check_modules(PKG_LIBGAIA2 libgaia2)
+         pkg_check_modules(GAIA2 libgaia2)
     endif ()
 endif (NOT WIN32)
 
-find_path(GAIA2_INCLUDE_DIR gaia2/gaia.h
-    ${PKG_LIBGAIA2_INCLUDE_DIRS}
-    /usr/include
-    /usr/local/include
-)
+if ( NOT GAIA2_FOUND )
+  find_path(GAIA2_INCLUDE_DIRS NAMES gaia2/gaia.h)
+  find_library(GAIA2_LIBRARIES NAMES gaia2)
 
-find_library(GAIA2_LIBRARIES
-    NAMES
-    gaia2 gaia2.dll
-    PATHS
-    ${PKG_LIBGAIA2_LIBRARY_DIRS}
-    /usr/lib
-    /usr/local/lib
-)
+  if ( NOT "${GAIA2_LIBRARIES}" STREQUAL "")
+    set (GAIA2_FOUND TRUE)
+    set (GAIA2_LINK_LIBRARIES ${GAIA2_LINK_LIBRARIES} ${GAIA2_LIBRARIES})
+  endif ()
+endif ()
 
-find_package_handle_standard_args(Gaia2 DEFAULT_MSG GAIA2_LIBRARIES GAIA2_INCLUDE_DIR)
+find_package_handle_standard_args(Gaia2 DEFAULT_MSG GAIA2_LIBRARIES GAIA2_INCLUDE_DIRS)
